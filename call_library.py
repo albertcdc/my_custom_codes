406072f9964e5f372d2b0ed48fefcc6345f98268
Albert Comellas <aco@wizata.com>
2025-01-09 12:28:42 +0100

    library version 0.1.1 with testing script

---
+++ call_library.py
@@ -0,0 +1,4 @@
+from mypythonlib import apply_sqrt
+import mypythonlib
+print(mypythonlib.__version__)
+print(apply_sqrt(38))
--- my_custom_library/setup.py
+++ my_custom_library/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='mypythonlib',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='My first Custom Python library',
     author='aco',
     url='https://github.com/albertcdc/my_custom_codes'
